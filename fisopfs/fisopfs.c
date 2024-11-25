2cf41e6343b48d7d693812ae4d710ad16177ab4e
matiasrebollo <mrebollo@fi.uba.ar>
2024-11-25 14:47:21 -0300

    Creo estructuras

--- fisopfs/fisopfs.c
+++ fisopfs/fisopfs.c
@@ -12,6 +12,37 @@
 
 #define DEFAULT_FILE_DISK "persistence_file.fisopfs"
 
+#define MAX_FILE_NAME 256
+#define MAX_FILES 128
+#define MAX_PATH 1024
+
+typedef struct archivo {
+	char nombre[MAX_FILE_NAME];
+	size_t tamanio;
+	void *data;
+	time_t fecha_creacion;
+	time_t fecha_modificacion;
+} archivo_t;
+
+// por ahora lo cree asi. No es un directorio flat porque un directorio puede tener subdirectorios, despues podemos cambiarlo para que sea un UNICO nivel de recursion.
+// se podria agregar un struct subdirectorio para los subdirectorios dentro del directorio raiz, y dejar struct directorio unicamente para el directorio raiz.
+typedef struct directorio {
+	char nombre[MAX_FILE_NAME];
+	struct directorio *padre;
+	struct directorio *subdirectorios[MAX_FILES];
+	archivo_t *archivos[MAX_FILES];
+	size_t cant_archivos;
+	size_t cant_directorios;
+	time_t fecha_creacion;
+	time_t fecha_modificacion;
+} directorio_t;
+
+typedef struct filesystem {
+	directorio_t *raiz;
+	size_t max_size;
+	size_t current_size;
+} filesystem_t;
+
 char *filedisk = DEFAULT_FILE_DISK;
 
 static int
