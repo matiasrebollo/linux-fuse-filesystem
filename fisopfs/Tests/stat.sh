67e7cd4943e2d345dd6e0696c2dbffd7e45ca478
agusgcasla <agusgd2003@gmail.com>
2024-12-01 20:19:06 -0300

    added stat test

---
+++ fisopfs/Tests/stat.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Variables
+MOUNT_POINT="./prueba"
+TEST_FILE="stat_test.txt"
+TEST_CONTENT="Hola, este es un test de stat."
+
+echo "Creando archivo '$TEST_FILE'..."
+echo "$TEST_CONTENT" > "$MOUNT_POINT/$TEST_FILE"
+
+echo "Leyendo stats de '$TEST_FILE' "
+touch "$MOUNT_POINT/$TEST_FILE"
+OUTPUT1=$(stat "$MOUNT_POINT/$TEST_FILE")
+sleep 1
+touch "$MOUNT_POINT/$TEST_FILE"
+OUTPUT2="$(stat "$MOUNT_POINT/$TEST_FILE")"
+
+if [ "$OUTPUT1" != "$OUTPUT2" ]; then
+    echo "✔️  El comando stat actualizó el tiempo de acceso correctamente."
+else
+    echo "❌  ERROR: El comando stat no actualizó correctamente."
+    exit 0
+fi
+
+# Limpieza
+#echo "Eliminando archivo '$TEST_FILE'..."
+#rm "$MOUNT_POINT/$TEST_FILE"
+
+echo "✔️  Test de cat completado con éxito."
